44c13571cf074c5aebededdde5021c70f0a013bf
TokaiCanada <tokaibrian@gmail.com>
2024-04-22 14:56:15 -0400

    added new brands, adjusted paths in en and fr with updated brands and added a 404 error page to each folder

---
+++ other/js/scripts.js
@@ -0,0 +1,39 @@
+var customSelects = document.getElementsByClassName("custom-select");
+for (var i = 0; i < customSelects.length; i++) {
+  var select = customSelects[i].getElementsByTagName("select")[0];
+  var selectedOption = select.options[select.selectedIndex];
+  var selectedText = selectedOption.textContent || selectedOption.innerText;
+
+  var selectSelected = document.createElement("div");
+  selectSelected.classList.add("select-selected");
+  selectSelected.innerHTML = selectedText;
+  customSelects[i].appendChild(selectSelected);
+
+  var selectItems = document.createElement("div");
+  selectItems.classList.add("select-items");
+  for (var j = 0; j < select.length; j++) {
+    var option = select.options[j];
+    var item = document.createElement("div");
+    item.innerHTML = option.textContent || option.innerText;
+    item.addEventListener("click", function() {
+      selectSelected.innerHTML = this.innerHTML;
+      select.value = this.innerHTML;
+      selectItems.style.display = "none";
+    });
+    selectItems.appendChild(item);
+  }
+  customSelects[i].appendChild(selectItems);
+
+  selectSelected.addEventListener("click", function() {
+    this.nextSibling.style.display = "block";
+  });
+
+  document.addEventListener("click", function(e) {
+    if (!e.target.matches(".select-selected")) {
+      var items = document.getElementsByClassName("select-items");
+      for (var k = 0; k < items.length; k++) {
+        items[k].style.display = "none";
+      }
+    }
+  });
+}
